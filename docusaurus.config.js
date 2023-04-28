a6f4227b9bc4d8bc865566ff7f195e59f8a6966b
zhongfa.ke <zhongfa.ke@shuyun.com>
2023-04-28 16:00:10 +0800

    feat: search add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,6 +141,21 @@ const config = {
         },
       },
     }),
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
 };
 
 module.exports = config;
